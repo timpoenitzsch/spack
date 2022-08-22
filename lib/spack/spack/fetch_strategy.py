80ca89c1aaecc2d40e9d79075c5a0fb0394f0052
Todd Gamblin <tgamblin@llnl.gov>
2022-08-21 18:51:13 -0700

    specs: include source provenance in `spec.json` and package hash
    We've included a package hash in Spack since #7193 for CI, and we started using it on
    the spec in #28504. However, what goes into the package hash is a bit opaque. Here's
    what `spec.json` looks like now:
    
    ```json
    {
      "spec": {
        "_meta": {
          "version": 3
        },
        "nodes": [
          {
            "name": "zlib",
            "version": "1.2.12",
            ...
            "patches": [
              "0d38234384870bfd34dfcb738a9083952656f0c766a0f5990b1893076b084b76"
            ],
            "package_hash": "pthf7iophdyonixxeed7gyqiksopxeklzzjbxtjrw7nzlkcqleba====",
            "hash": "ke4alug7ypoxp37jb6namwlxssmws4kp"
          }
        ]
      }
    }
    ```
    
    The `package_hash` there is a hash of the concatenation of:
    
    * A canonical hash of the `package.py` recipe, as implemented in #28156;
    * `sha256`'s of patches applied to the spec; and
    * Archive `sha256` sums of archives or commits/revisions of repos used to build the spec.
    
    There are some issues with this: patches are counted twice in this spec (in `patches`
    and in the `package_hash`), the hashes of sources used to build are conflated with the
    `package.py` hash, and we don't actually include resources anywhere.
    
    With this PR, I've expanded the package hash out in the `spec.json` body. Here is the
    "same" spec with the new fields:
    
    ```json
    {
      "spec": {
        "_meta": {
          "version": 3
        },
        "nodes": [
          {
            "name": "zlib",
            "version": "1.2.12",
            ...
            "package_hash": "6kkliqdv67ucuvfpfdwaacy5bz6s6en4",
            "sources": [
              {
                "type": "archive",
                "sha256": "91844808532e5ce316b3c010929493c0244f3d37593afd6de04f71821d5136d9"
              }
            ],
            "patches": [
              "0d38234384870bfd34dfcb738a9083952656f0c766a0f5990b1893076b084b76"
            ],
            "hash": "ts3gkpltbgzr5y6nrfy6rzwbjmkscein"
          }
        ]
      }
    }
    ```
    
    Now:
    
    * Patches and archive hashes are no longer included in the `package_hash`;
    * Artifacts used in the build go in `sources`, and we tell you their checksum in the `spec.json`;
    * `sources` will include resources for packages that have it;
    * Patches are the same as before -- but only represented once; and
    * The `package_hash` is a base32-encoded `sha1`, like other hashes in Spack, and it only
      tells you that the `package.py` changed.
    
    The behavior of the DAG hash (which includes the `package_hash`) is basically the same
    as before, except now resources are included, and we can see differences in archives and
    resources directly in the `spec.json`
    
    Note that we do not need to bump the spec meta version on this, as past versions of
    Spack can still read the new specs; they just will not notice the new fields (which is
    fine, since we currently do not do anything with them).
    
    Among other things, this will more easily allow us to convert Spack specs to SBOM and
    track relevant security information (like `sha256`'s of archives). For example, we could
    do continuous scanning of a Spack installation based on these hashes, and if the
    `sha256`'s become associated with CVE's, we'll know we're affected.
    
    - [x] Add a method, `spec_attrs()` to `FetchStrategy` that can be used to describe a
          fetcher for a `spec.json`.
    
    - [x] Simplify the way package_hash() is handled in Spack. Previously, it was handled as
          a special-case spec hash in `hash_types.py`, but it really doesn't belong there.
          Now, it's handled as part of `Spec._finalize_concretization()` and `hash_types.py`
          is much simpler.
    
    - [x] Change `PackageBase.content_hash()` to `PackageBase.artifact_hashes()`, and
          include more information about artifacts in it.
    
    - [x] Update package hash tests and make them check for artifact and resource hashes.
    
    Signed-off-by: Todd Gamblin <tgamblin@llnl.gov>

--- lib/spack/spack/fetch_strategy.py
+++ lib/spack/spack/fetch_strategy.py
@@ -49,6 +49,7 @@ import spack.url
 import spack.util.archive
 import spack.util.crypto as crypto
 import spack.util.git
+import spack.util.spack_yaml as syaml
 import spack.util.url as url_util
 import spack.util.web as web_util
 import spack.version
@@ -122,6 +123,20 @@ class FetchStrategy:
         self.cache_enabled = not kwargs.pop("no_cache", False)
 
         self.package = None
+
+    def spec_attrs(self):
+        """Create a dictionary of attributes that describe this fetch strategy for a Spec.
+
+        This is included in the serialized Spec format to store provenance (like hashes).
+        """
+        attrs = syaml.syaml_dict()
+        if self.url_attr:
+            attrs["type"] = "archive" if self.url_attr == "url" else self.url_attr
+        for attr in self.optional_attrs:
+            value = getattr(self, attr, None)
+            if value:
+                attrs[attr] = value
+        return attrs
 
     def set_package(self, package):
         self.package = package
@@ -265,6 +280,16 @@ class URLFetchStrategy(FetchStrategy):
         self.extra_options: dict = kwargs.get("fetch_options", {})
         self._curl: Optional[Executable] = None
         self.extension: Optional[str] = kwargs.get("extension", None)
+
+    def spec_attrs(self):
+        attrs = super(URLFetchStrategy, self).spec_attrs()
+        if self.digest:
+            try:
+                hash_type = spack.util.crypto.hash_algo_for_digest(self.digest)
+            except ValueError:
+                hash_type = "digest"
+            attrs[hash_type] = self.digest
+        return attrs
 
     @property
     def curl(self) -> Executable:
